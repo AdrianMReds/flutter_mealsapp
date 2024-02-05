3cd04928e94a4b397d241c81efd6295eb16acb9c
AdrianMReds <adrianmontemayor.r@gmail.com>
2024-02-05 16:41:48 -0600

    Filtered meals provider

--- lib/screens/meal_detail.dart
+++ lib/screens/meal_detail.dart
@@ -13,6 +13,10 @@ class MealDetailScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final favoriteMeals = ref.watch(favoriteMealsProvider);
+
+    final isFavorite = favoriteMeals.contains(meal);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title),
@@ -29,7 +33,7 @@ class MealDetailScreen extends ConsumerWidget {
                       ? '${meal.title} added as favorite'
                       : '${meal.title} removed as favorite')));
             },
-            icon: Icon(Icons.star_border),
+            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
           ),
         ],
       ),
